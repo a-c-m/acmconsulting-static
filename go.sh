33afb60b3c22ff8672023f44e8cba30f62343726
Alex McFadyen <alex.mcfadyen@bengroup.com>
2021-04-27 12:20:43 +0100

    https for images

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gssg --domain http://localhost:2368 --url http://www.acmconsulting.eu --dest .
+gssg --domain http://localhost:2368 --url https://www.acmconsulting.eu --dest .
 git add .
 git commit -m "$1"
 git push
