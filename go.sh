dd5583f6ea85fd0d76cf3f93b8fecb49b6f0fe46
Alex McFadyen <alex.mcfadyen@bengroup.com>
2021-05-05 10:24:55 +0100

    microdosing wellness

--- go.sh
+++ go.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-node -v
-gssg --domain http://localhost:2368 --url https://www.acmconsulting.eu --dest .
+/Users/alex/.nvm/versions/node/v12.14.1/bin/gssg --domain http://localhost:2368 --url https://www.acmconsulting.eu --dest .
 git add .
 git commit -m "$1"
 git push
